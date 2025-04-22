77841b562bc9b9f562f0d5e988b621c5035838b8
aarishfaruqii <aarishfaruqi1212@gmail.com>
2025-04-22 18:34:26 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -151,9 +151,9 @@ def process_image():
         print(f"Error processing image: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    # Get port from environment variable, default to 5000 if not found
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
 
     
